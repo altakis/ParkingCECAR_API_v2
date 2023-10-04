5550825935f906ae9a6b4a2912a24ceee3c4ef51
ReadingShades <42413498+ReadingShades@users.noreply.github.com>
2023-10-03 23:58:53 -0500

    Fix redimensioned settings package base folder detph

--- backend/core/settings/constants.py
+++ backend/core/settings/constants.py
@@ -1,8 +1,14 @@
 import os.path
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / "subdir".
-BASE_DIR = Path(__file__).resolve().parent.parent
+""" Build paths inside the project like this: BASE_DIR / "subdir".
+The first .parent identifies the current folder.
+The second creates a path to the parent directory in this case core.
+The third and final points to the project root folder.
+If this was a module file instead of a module package then only
+two calls of .parent would have been enough.
+"""
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Define the directory where log files will be stored
 LOGGING_DIR = os.path.join(BASE_DIR, "logs")
