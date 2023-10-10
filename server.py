6fe8514449b4cfbbc9179e06fb1d30f5341084ff
ReadingShades <42413498+ReadingShades@users.noreply.github.com>
2023-10-10 10:46:57 -0500

    Update Dockerfiles

---
+++ server.py
@@ -0,0 +1,11 @@
+"""Simple Flask app to make test calls if needed
+"""
+from flask import Flask
+server = Flask(__name__)
+
+@server.route("/")
+def hello():
+   return "Hello World!"
+
+if __name__ == "__main__":
+   server.run(host='0.0.0.0')
