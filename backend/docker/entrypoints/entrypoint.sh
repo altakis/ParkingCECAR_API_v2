d8410657b711536eb31eb51600f577c5f54db17d
ReadingShades <42413498+ReadingShades@users.noreply.github.com>
2023-10-10 18:19:23 -0500

    Update Dockerfiles

--- backend/docker/entrypoints/entrypoint.sh
+++ backend/docker/entrypoints/entrypoint.sh
@@ -3,4 +3,8 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py test
 
+python -m celery -A core worker -l DEBUG -P gevent &
+
+python -m manage runserver 0.0.0.0:8000
+
 exec "$@"
